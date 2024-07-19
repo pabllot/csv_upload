f5e0ddaea4b1c0b35ddd45df967eb6effc9db947
Pabllot07 <pabllot07@outlook.com>
2024-07-19 18:35:27 -0300

    fe first config tests

---
+++ frontend/jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transformIgnorePatterns: ["node_modules/(?!(your-module-to-transform|another-module))"],
+};
+
+export default config;
