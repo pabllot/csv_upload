f8c972625d8513fbb46deea655ab74789cd6179c
Pabllot07 <pabllot07@outlook.com>
2024-07-20 02:24:54 -0300

    styles done

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -3,10 +3,10 @@ import styles from "./styles.module.scss";
 export const Card = ({ user }: any) => {
   return (
     <div key={user.id} className={styles.wrapper}>
-      <p>Name:</p> <strong> {user.name}</strong>
-      <p>City:</p> <strong> {user.city}</strong>
-      <p>country:</p> <strong> {user.country}</strong>
-      <p>favorite_sport:</p> <strong> {user.favorite_sport}</strong>
+      <p>Name</p> <strong> {user.name}</strong>
+      <p>City</p> <strong> {user.city}</strong>
+      <p>country</p> <strong> {user.country}</strong>
+      <p>favorite_sport</p> <strong> {user.favorite_sport}</strong>
     </div>
   );
 };
