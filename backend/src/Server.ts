c86ab3204812d25ea9d80956d5cfdddcd2e240e5
Pabllot07 <pabllot07@outlook.com>
2024-07-20 11:58:25 -0300

    refactor: prod mode

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import https from "https";
+import fs from "fs";
 
 import filesRoutes from "./routes/files";
 import usersRoutes from "./routes/users";
@@ -12,10 +14,25 @@ app.use(cors());
 app.use("/api/users", usersRoutes);
 app.use("/api/files", filesRoutes);
 
-if (process.env.NODE_ENV !== "test") {
-  app.listen(port, () => {
-    console.log(`Backend server running on port ${port}`);
-  });
-}
+// if (process.env.NODE_ENV !== "test") {
+//   app.listen(port, () => {
+//     console.log(`Backend server running on port ${port}`);
+//   });
+// }
+
+const PORT = 3004;
+const KEY = fs.readFileSync("/etc/letsencrypt/live/seucardapioaqui.com-0001/privkey.pem");
+const CA = fs.readFileSync("/etc/letsencrypt/live/seucardapioaqui.com-0001/chain.pem");
+const CERT = fs.readFileSync("/etc/letsencrypt/live/seucardapioaqui.com-0001/cert.pem");
+
+const https_options = {
+  key: KEY,
+  ca: CA,
+  cert: CERT,
+};
+
+https.createServer(https_options, app).listen(PORT, "", undefined, () => {
+  console.log(`CSV Upload File: ${PORT}`);
+});
 
 export default app;
