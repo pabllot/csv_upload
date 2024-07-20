a4c8a9e69211bfbeca2fa24120b23121bf9194ff
Pabllot07 <pabllot07@outlook.com>
2024-07-20 11:55:28 -0300

    refactor: dev/prod variables

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const PROD = "https://seucardapioaqui:3004";
+const DEV = "http://localhost:3000";
+
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: DEV,
 });
